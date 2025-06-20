4bf2876f57219b7e3c90e904ba2c5d3bf3830a96
Madhu <nmadhu1995@gmail.com>
2025-06-20 18:11:36 +0800

    login page in progress

--- Frontend/Expense_Tracker/src/App.jsx
+++ Frontend/Expense_Tracker/src/App.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+} from "react-router-dom";
+import Login from "./Pages/Auth/Login";
+import SignUp from "./Pages/Auth/SignUp";
+import Home from "./Pages/Dashboard/Home";
+import Income from "./Pages/Dashboard/Income";
+import Expense from "./Pages/Dashboard/Expense";
 
 const App = () => {
-    return <div>App</div>;
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Root />} />
+                <Route path="/login" exact element={<Login />} />
+                <Route path="/signup" exact element={<SignUp />} />
+                <Route path="/dashboard" exact element={<Home />} />
+                <Route path="/income" exact element={<Income />} />
+                <Route path="/expense" exact element={<Expense />} />
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
+
+const Root = () => {
+    const isAuthenticate = localStorage.get("token");
+    return isAuthenticate ? (
+        <Navigate to={"/dashboard"} />
+    ) : (
+        <Navigate to={"/login"} />
+    );
+};
--- Frontend/Expense_Tracker/src/Pages/Auth/Login.jsx
+++ Frontend/Expense_Tracker/src/Pages/Auth/Login.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import AuthLayout from "../../Components/Layouts/AuthLayout";
+import { useNavigate } from "react-router-dom";
+import Input from "../../Components/Inputs/Input";
 
 const Login = () => {
-    return <div>Login</div>;
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const navigate = useNavigate();
+
+    const handleLogin = async (e) => {};
+
+    return (
+        <AuthLayout>
+            <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
+                <h3 className="text-xl font-semibold text-black">
+                    Welcome back!
+                </h3>
+                <p className="text-xs text-slate-700 mt-[px] mb-6">
+                    Please enter your details to login
+                </p>
+
+                <form onSubmit={handleLogin}>
+                    <Input
+                        value={email}
+                        onChange={({ target }) => setEmail(target.value)}
+                        label="Email Address"
+                        placeholder="aria@gmail.com"
+                        type="text"
+                    />
+                </form>
+            </div>
+        </AuthLayout>
+    );
 };
 
 export default Login;
